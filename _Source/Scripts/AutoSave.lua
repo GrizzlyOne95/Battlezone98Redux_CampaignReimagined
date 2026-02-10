423dfeafb3584c2620edcefe7aee823a22b72f59
GrizzlyOne95 <143971298+GrizzlyOne95@users.noreply.github.com>
2026-02-10 14:42:56 -0600

    feat: Add AutoSave.lua for programmatic save file generation and auto-saving functionality in Battlezone 98 Redux.

--- _Source/Scripts/AutoSave.lua
+++ _Source/Scripts/AutoSave.lua
@@ -77,7 +77,6 @@ function AutoSave.Update()
     if not AutoSave.Config.enabled then return end
     
     AutoSave.timer = AutoSave.timer + GetTimeStep()
-    
     if AutoSave.timer >= AutoSave.Config.autoSaveInterval then
         AutoSave.CreateSave()
         AutoSave.timer = 0.0
@@ -85,183 +84,133 @@ function AutoSave.Update()
 end
 
 ---
---- Internal: Save File Generation
+--- Internal Writers
 ---
 
 function AutoSave._WriteSaveFile(file)
-    -- 1. Write header
-    AutoSave._WriteHeader(file)
+    [cite_start]-- Write Header Information based on game1.sav [cite: 1]
+    file:Writeln("version [1] = 2016")
+    file:Writeln("binarySave [1] = false")
+    file:Writeln("msn_filename = " .. GetMissionFilename())
+    file:Writeln("seq_count [1] = 1000") -- Placeholder high count
+    file:Writeln("missionSave [1] = false")
+    file:Writeln("runType [1] = 0")
+    [cite_start]file:Writeln("nPlayerSide = usa") -- Default to USA side [cite: 3]
+    file:Writeln("TerrainName = " .. GetMapName())
+    [cite_start]file:Writeln("start_time [1] = " .. tostring(GetTime())) [cite: 4]
     
-    -- 2. Write all game objects
-    local objectCount = 0
-    for obj in AllGameObjects() do
+    -- Iterate through all game objects
+    for i = 1, 1024 do
+        local obj = GetHandle(i)
         if IsValid(obj) then
             AutoSave._WriteGameObject(file, obj)
-            objectCount = objectCount + 1
         end
     end
-    
-    print("AutoSave: Serialized " .. objectCount .. " objects")
-end
-
-function AutoSave._WriteHeader(file)
-    file:Writeln("version [1] =")
-    file:Writeln("2016")
-    file:Writeln("binarySave [1] =")
-    file:Writeln("false")
-    
-    -- Mission filename
-    local missionName = GetMapTRNFilename() or "misn01.bzn"
-    missionName = string.gsub(missionName, "%.trn$", ".bzn")
-    file:Writeln("msn_filename = " .. missionName)
-    
-    -- Sequence count (will be updated later if needed)
-    file:Writeln("seq_count [1] =")
-    file:Writeln("1000")  -- Placeholder
-    
-    file:Writeln("missionSave [1] =")
-    file:Writeln("false")
-    file:Writeln("runType [1] =")
-    file:Writeln("0")
-    
-    -- Save description (hex encoded timestamp)
-    local desc = AutoSave._GenerateSaveDescription()
-    file:Writeln("saveGameDesc = " .. desc)
-    
-    file:Writeln("nPlayerSide = usa")  -- TODO: Make dynamic
-    file:Writeln("nMissionStatus [1] =")
-    file:Writeln("0")
-    file:Writeln("nOldMissionMode [1] =")
-    file:Writeln("0")
-    
-    local terrainName = string.gsub(missionName, "%.bzn$", "")
-    file:Writeln("TerrainName = " .. terrainName)
-    
-    file:Writeln("start_time [1] =")
-    file:Writeln(tostring(GetTime()))
-    
-    file:Writeln("size [1] =")
-    file:Writeln("63")  -- Placeholder
-end
-
-function AutoSave._GenerateSaveDescription()
-    -- Generate hex-encoded save description
-    -- Format: "Auto-Save <timestamp>" encoded as hex + null padding
-    local text = "Auto-Save " .. os.date("%m/%d/%Y %I:%M:%S %p")
-    local hex = ""
-    for i = 1, #text do
-        hex = hex .. string.format("%02x", string.byte(text, i))
-    end
-    
-    -- Pad with zeros to match engine format (512 chars)
-    while #hex < 512 do
-        hex = hex .. "00"
-    end
-    
-    return hex
 end
 
 function AutoSave._WriteGameObject(file, obj)
-    file:Writeln("[GameObject]")
+    [cite_start]file:Writeln("[GameObject]") [cite: 5]
+    [cite_start]file:Writeln("PrjID [1] = " .. GetCfg(obj)) [cite: 5]
+    [cite_start]file:Writeln("team [1] = " .. tostring(GetTeamNum(obj))) [cite: 7]
+    [cite_start]file:Writeln("label = " .. (GetLabel(obj) or "")) [cite: 7]
     
-    -- Basic identity
-    local odf = GetOdf(obj)
-    if odf then
-        file:Writeln("PrjID [1] =")
-        file:Writeln(odf)
-    end
-    
-    -- Sequence number (unique ID)
-    local seqno = GetSeqNo(obj)
-    if seqno then
-        file:Writeln("seqno [1] =")
-        file:Writeln(tostring(seqno))
-    end
-    
-    -- Position
-    local pos = GetPosition(obj)
-    if pos then
-        file:Writeln("pos [1] =")
-        file:Writeln("  x [1] =")
-        file:Writeln(tostring(pos.x))
-        file:Writeln("  y [1] =")
-        file:Writeln(tostring(pos.y))
-        file:Writeln("  z [1] =")
-        file:Writeln(tostring(pos.z))
-    end
-    
-    -- Team
-    local team = GetTeamNum(obj)
-    if team then
-        file:Writeln("team [1] =")
-        file:Writeln(tostring(team))
-    end
-    
-    -- Label
-    local label = GetLabel(obj)
-    if label and label ~= "" then
-        file:Writeln("label = " .. label)
-    end
-    
-    -- Is user (player)
-    local isUser = (obj == GetPlayerHandle())
-    file:Writeln("isUser [1] =")
-    file:Writeln(isUser and "1" or "0")
-    
-    -- Transform matrix (simplified - identity for now)
-    file:Writeln("transform [1] =")
+    [cite_start]-- Position and Rotation Fix [cite: 8, 11]
     AutoSave._WriteTransform(file, obj)
     
-    -- Health
-    local curHealth = GetCurHealth(obj)
-    local maxHealth = GetMaxHealth(obj)
-    if curHealth and maxHealth then
-        file:Writeln("curHealth [1] =")
-        file:Writeln(tostring(curHealth))
-        file:Writeln("maxHealth [1] =")
-        file:Writeln(tostring(maxHealth))
-        file:Writeln("healthRatio [1] =")
-        file:Writeln(tostring(maxHealth > 0 and curHealth / maxHealth or 0))
-    end
+    [cite_start]-- Physics Momentum Fix [cite: 18, 19]
+    AutoSave._WritePhysics(file, obj)
     
-    -- Ammo
-    local curAmmo = GetCurAmmo(obj)
-    local maxAmmo = GetMaxAmmo(obj)
-    if curAmmo and maxAmmo then
-        file:Writeln("curAmmo [1] =")
-        file:Writeln(tostring(curAmmo))
-        file:Writeln("maxAmmo [1] =")
-        file:Writeln(tostring(maxAmmo))
-        file:Writeln("ammoRatio [1] =")
-        file:Writeln(tostring(maxAmmo > 0 and curAmmo / maxAmmo or 0))
-    end
+    -- Stats
+    AutoSave._WriteHealthAmmo(file, obj)
     
-    -- Pilot
+    [cite_start]-- AI State Fix [cite: 27, 28]
+    AutoSave._WriteAIState(file, obj)
+    
+    [cite_start]-- Pilot Information [cite: 31]
     local pilot = GetPilotClass(obj)
     if pilot and pilot ~= "" then
-        file:Writeln("curPilot [1] =")
-        file:Writeln(pilot)
+        file:Writeln("curPilot [1] = " .. pilot)
     end
 end
 
 function AutoSave._WriteTransform(file, obj)
-    -- Write identity transform for now
-    -- TODO: Extract actual rotation matrix if possible
-    file:Writeln("  right_x [1] = 1")
-    file:Writeln("  right_y [1] = 0")
-    file:Writeln("  right_z [1] = 0")
-    file:Writeln("  up_x [1] = 0")
-    file:Writeln("  up_y [1] = 1")
-    file:Writeln("  up_z [1] = 0")
-    file:Writeln("  front_x [1] = 0")
-    file:Writeln("  front_y [1] = 0")
-    file:Writeln("  front_z [1] = 1")
+    [cite_start]local transform = GetTransform(obj) [cite: 8]
+    if transform then
+        file:Writeln("transform [1] =")
+        [cite_start]-- Orientation Matrix [cite: 9, 10, 11]
+        file:Writeln("  right_x [1] = " .. tostring(transform.right.x))
+        file:Writeln("  right_y [1] = " .. tostring(transform.right.y))
+        file:Writeln("  right_z [1] = " .. tostring(transform.right.z))
+        file:Writeln("  up_x [1] = " .. tostring(transform.up.x))
+        file:Writeln("  up_y [1] = " .. tostring(transform.up.y))
+        file:Writeln("  up_z [1] = " .. tostring(transform.up.z))
+        file:Writeln("  front_x [1] = " .. tostring(transform.front.x))
+        file:Writeln("  front_y [1] = " .. tostring(transform.front.y))
+        file:Writeln("  front_z [1] = " .. tostring(transform.front.z))
+        [cite_start]-- Origin Position [cite: 11, 12]
+        file:Writeln("  posit_x [1] = " .. tostring(transform.posit.x))
+        file:Writeln("  posit_y [1] = " .. tostring(transform.posit.y))
+        file:Writeln("  posit_z [1] = " .. tostring(transform.posit.z))
+    end
+end
+
+function AutoSave._WritePhysics(file, obj)
+    [cite_start]local vel = GetVelocity(obj) [cite: 17, 18]
+    [cite_start]local omega = GetOmega(obj) [cite: 19]
+    [cite_start]local v_mag = math.sqrt(vel.x^2 + vel.y^2 + vel.z^2) [cite: 16]
+
+    file:Writeln(" v_mag [1] = " .. tostring(v_mag))
+    [cite_start]file:Writeln(" v_mag_inv [1] = " .. tostring(v_mag > 0 and 1/v_mag or 1e+030)) [cite: 16]
     
-    local pos = GetPosition(obj)
-    if pos then
-        file:Writeln("  posit_x [1] = " .. tostring(pos.x))
-        file:Writeln("  posit_y [1] = " .. tostring(pos.y))
-        file:Writeln("  posit_z [1] = " .. tostring(pos.z))
+    file:Writeln(" v [1] =")
+    file:Writeln("  x [1] = " .. tostring(vel.x))
+    file:Writeln("  y [1] = " .. tostring(vel.y))
+    file:Writeln("  z [1] = " .. tostring(vel.z))
+    
+    file:Writeln(" omega [1] =")
+    file:Writeln("  x [1] = " .. tostring(omega.x))
+    file:Writeln("  y [1] = " .. tostring(omega.y))
+    file:Writeln("  z [1] = " .. tostring(omega.z))
+end
+
+function AutoSave._WriteHealthAmmo(file, obj)
+    [cite_start]-- Health [cite: 25]
+    local curHealth = GetCurHealth(obj)
+    local maxHealth = GetMaxHealth(obj)
+    if curHealth and maxHealth then
+        file:Writeln("curHealth [1] = " .. tostring(curHealth))
+        file:Writeln("maxHealth [1] = " .. tostring(maxHealth))
+        file:Writeln("healthRatio [1] = " .. tostring(maxHealth > 0 and curHealth / maxHealth or 0))
+    end
+    
+    [cite_start]-- Ammo [cite: 26]
+    local curAmmo = GetCurAmmo(obj)
+    local maxAmmo = GetMaxAmmo(obj)
+    if curAmmo and maxAmmo then
+        file:Writeln("curAmmo [1] = " .. tostring(curAmmo))
+        file:Writeln("maxAmmo [1] = " .. tostring(maxAmmo))
+        file:Writeln("ammoRatio [1] = " .. tostring(maxAmmo > 0 and curAmmo / maxAmmo or 0))
+    end
+end
+
+function AutoSave._WriteAIState(file, obj)
+    [cite_start]local cmd = GetCurrentCommand(obj) [cite: 27]
+    [cite_start]-- .sav format requires two command blocks (Primary and Secondary) [cite: 28, 29]
+    for i = 1, 2 do
+        if i == 1 and cmd then
+            file:Writeln("priority [1] = " .. tostring(cmd.priority or 0))
+            file:Writeln("what = " .. string.format("%08x", cmd.command or 0))
+            file:Writeln("who [1] = " .. tostring(cmd.who or 0))
+            file:Writeln("where = " .. string.format("%08x", cmd.where or 0))
+            file:Writeln("param [1] = " .. tostring(cmd.param or ""))
+        else
+            [cite_start]-- Empty secondary command block [cite: 29, 30]
+            file:Writeln("priority [1] = 0")
+            file:Writeln("what = 00000000")
+            file:Writeln("who [1] = 0")
+            file:Writeln("where = 00000000")
+            file:Writeln("param [1] = ")
+        end
     end
 end
 
