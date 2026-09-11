086ba03d702ba8a113770c1fd0d449bc7861a000
GrizzlyOne95 <iestuart95@hotmail.com>
2026-09-10 23:10:06 -0500

    Remove trash files and reorganize

--- Scripts/OpenShimManifest.lua
+++ Scripts/OpenShimManifest.lua
@@ -2,14 +2,14 @@
 -- Do not edit payload metadata by hand.
 return {
     formatVersion = 2,
-    version = "1.0.0.22",
-    sha256 = "37362cce0755bb35e398ab5f92edc146acbf0f19fc97b6da061d86fe7fe60cd3",
-    size = 2938880,
+    version = "1.0.0.27",
+    sha256 = "e3c7833f2e436b52357185c8634e3dd1c5db38b4ad527693dbacdac27aec6c8f",
+    size = 2939904,
     architecture = "x86",
     payloads = {
-        winmm = { source = "winmm.dll", destination = "winmm.dll", sha256 = "37362cce0755bb35e398ab5f92edc146acbf0f19fc97b6da061d86fe7fe60cd3", size = 2938880, version = "1.0.0.22", architecture = "x86" },
+        winmm = { source = "winmm.dll", destination = "winmm.dll", sha256 = "e3c7833f2e436b52357185c8634e3dd1c5db38b4ad527693dbacdac27aec6c8f", size = 2939904, version = "1.0.0.27", architecture = "x86" },
         network = { source = "openshim_net.ini.payload", destination = "net.ini", sha256 = "25853671cceaf7273ba6b43bf0fec1bf34ed0298e8c238cb6ec68a4303e99056", size = 13245 },
         patches = { source = "openshim_patches.json.payload", destination = "scripts\\patches.json", sha256 = "f24a9593fdabd28ff396a6724afa15b5f43abc7aafb1e7791583f13883518c7c", size = 33599 },
-        playerConfig = { source = "openshim.ini.payload", destination = "openshim.ini", sha256 = "0ceac84bc0bcc0261d3f6a910a3623e19eff09b080d2bd3daea0b7976a643a02", size = 17706, overwrite = false },
+        playerConfig = { source = "openshim.ini.payload", destination = "openshim.ini", sha256 = "08b072ed5f002d242c76fc89ac7e2dcab8e3b231aea5e194d5f75a596aca7976", size = 17887, overwrite = false },
     },
 }
